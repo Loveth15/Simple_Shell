4530f817045ea66e7b493292dcaf9efba2d83e22
Loveth <ogoloveth73@gmail.com>
2023-12-15 23:37:42 +0100

    Task )

--- _interactive.c
+++ _interactive.c
@@ -1,69 +1,74 @@
 #include "shell.h"
 
 /**
-*interactive - returns true if shell is interactive mode
-*@shell: pointer to the shell structure
-*Return: Always return 1 if interactive mode, 0 otherwise
-*/
-int interactive(shell_t *shell)
+ * interactive - This function returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
 {
-return (isatty(STDIN_FILENO) && shell->readfd <= 2);
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
-*is_delimeter - Checks if a character is a delimiter.
-*@ch: The character to check.
-*@delimiter: The delimiter string.
-*Return: Always return 1 if true, 0 if false.
-*/
-int is_delimeter(char ch, char *delimeter)
+ * is_delim - This program checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
 {
-while (*delimeter)
-if (*delimeter++ == ch)
-return (1);
-return (0);
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
 }
 
 /**
-*_isalpha - Checks if a character is alphabetic.
-*@ch: The character to check.
-*Return: Always return 1 if @ch is alphabetic, 0 otherwise.
-*/
-int _isalpha(int ch)
+ *_isalpha - This program checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
+ */
+
+int _isalpha(int c)
 {
-if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
-return (1);
-else
-return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
 
 /**
-*_atoi - Converts a string to an integer.
-*@str: The string to be converted.
-*Return: The converted number, or 0 if no numbers in the string.
-*/
-int _atoi(char *str)
-{
-int a, sign = 1, flag = 0, output;
-unsigned int result = 0;
+ *_atoi - This program converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
+ */
 
-for (a = 0; str[a] != '\0' && flag != 2; a++)
+int _atoi(char *s)
 {
-if (str[a] == '-')
-sign *= -1;
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
 
-if (str[a] >= '0' && str[a] <= '9')
-{
-flag = 1;
-result *= 10;
-result += (str[a] - '0');
-}
-else if (flag == 1)
-flag = 2;
-}
-if (sign == -1)
-output = -result;
-else
-output = result;
-return (output);
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
 }
